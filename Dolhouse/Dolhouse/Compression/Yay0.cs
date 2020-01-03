544c245838c8a5f29072979bd1ade8413d11abbe
nya <opeyx@pm.me>
2020-01-04 00:57:02 +0100

    Added Yay0 compression code.

--- Dolhouse/Dolhouse/Compression/Yay0.cs
+++ Dolhouse/Dolhouse/Compression/Yay0.cs
@@ -1,20 +1,181 @@
 ﻿using Dolhouse.Binary;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Dolhouse.Compression
 {
 
     /// <summary>
     /// Yay0 Compression
-    /// TODO: Add compression functionality + Document properly.
     /// </summary>
     public static class Yay0
     {
 
         /// <summary>
-        /// Decompressing Yay0 compressed data. Full credits for this snippet goes to Daniel McCarthy:
+        /// Compressing data with Yay0. Full credits for this snippet goes to Cuyler36:
+        /// https://github.com/Cuyler36/GCNToolKit/blob/master/GCNToolKit/Formats/Compression/Yay0.cs
+        /// </summary>
+        /// <param name="stream">Stream containing the data to Yay0 compress.</param>
+        /// <returns>Compressed data as a stream.</returns>
+        public static Stream Compress(Stream stream)
+        {
+
+            // Define byte array to hold our data.
+            byte[] data;
+
+            // Temporary create a memory stream, dispose it after.
+            using (MemoryStream ms = new MemoryStream())
+            {
+
+                // Copy the contents of the stream into the memory stream.
+                stream.CopyTo(ms);
+
+                // Set the byte array to the content of the memory stream.
+                data = ms.ToArray();
+            }
+
+            const int OFSBITS = 12;
+            int decPtr = 0;
+
+            // Setup mask buffer related variables.
+            uint maskMaxSize = (uint)(data.Length + 32) >> 3; // 1 bit per byte
+            uint maskBitCount = 0, mask = 0;
+            uint[] maskBuffer = new uint[maskMaxSize / 4];
+            int maskPtr = 0;
+
+            // Initialize the links buffer related variables.
+            uint linkMaxSize = (uint)data.Length / 2;
+            ushort linkOffset = 0;
+            ushort[] linkBuffer = new ushort[linkMaxSize];
+            int linkPtr = 0;
+            ushort minCount = 3, maxCount = 273;
+
+            // Initialize the chunk buffer related variables.
+            uint chunkMaxSize = (uint)data.Length;
+            byte[] chunkBuffer = new byte[chunkMaxSize];
+            int chunkPtr = 0;
+
+            int windowPtr = decPtr;
+            int windowLen = 0, length, maxlen;
+
+            // Loop through the data we're going to compress.
+            while (decPtr < data.Length)
+            {
+
+                if (windowLen >= (1 << OFSBITS))
+                {
+                    windowLen = windowLen - (1 << OFSBITS);
+                    windowPtr = decPtr - windowLen;
+                }
+
+                if ((data.Length - decPtr) < maxCount)
+                    maxCount = (ushort)(data.Length - decPtr);
+
+                // Scan through the window.
+                maxlen = 0;
+                for (int i = 0; i < windowLen; i++)
+                {
+
+                    for (length = 0; length < (windowLen - i) && length < maxCount; length++)
+                    {
+                        if (data[decPtr + length] != data[windowPtr + length + i]) break;
+                    }
+
+                    if (length > maxlen)
+                    {
+                        maxlen = length;
+                        linkOffset = (ushort)(windowLen - i);
+                    }
+                }
+                length = maxlen;
+
+                mask <<= 1;
+                if (length >= minCount) // Add Link
+                {
+
+                    ushort link = (ushort)((linkOffset - 1) & 0x0FFF);
+
+                    if (length < 18)
+                    {
+                        link |= (ushort)((length - 2) << 12);
+                    }
+                    else
+                    {
+                        // Store the current count as a chunk.
+                        chunkBuffer[chunkPtr++] = (byte)(length - 18);
+                    }
+
+                    linkBuffer[linkPtr++] = (ushort)((link << 8) | (link >> 8));
+                    decPtr += length;
+                    windowLen += length;
+                }
+                else
+                {
+
+                    // Add single byte, increase Window.
+                    chunkBuffer[chunkPtr++] = data[decPtr++];
+                    windowLen++;
+                    mask |= 1;
+                }
+
+                maskBitCount++;
+                if (maskBitCount == 32)
+                {
+                    // Store the current mask in the mask buffer.
+                    maskBuffer[maskPtr] = ((mask << 24) | ((mask >> 24) & 0xFF) | ((mask & 0xFF00) << 8) | ((mask >> 8) & 0xFF00));
+                    maskPtr++;
+                    maskBitCount = 0;
+                }
+            }
+
+            // Flush the current mask from the mask buffer.
+            if (maskBitCount > 0)
+            {
+                mask <<= (int)(32 - maskBitCount);
+
+                // Store the current mask in the mask buffer.
+                maskBuffer[maskPtr] = ((mask << 24) | ((mask >> 24) & 0xFF) | ((mask & 0xFF00) << 8) | ((mask >> 8) & 0xFF00));
+                maskPtr++;
+            }
+
+            // Calculate the size of each section.
+            uint maskSize = (uint)maskPtr * sizeof(uint);
+            uint linkSize = (uint)linkPtr * sizeof(ushort);
+            uint chunkSize = (uint)chunkPtr * sizeof(byte);
+
+            // Calculate the total size of the compressed file.
+            uint encodedSize = 0x10 + maskSize + linkSize + chunkSize;
+
+            // Setup a buffer we'll write to.
+            byte[] buffer = new byte[encodedSize];
+
+            // Define a binary writer to write with.
+            DhBinaryWriter bw = new DhBinaryWriter(new MemoryStream(buffer), DhEndian.Big);
+
+            // Calculate the offset for each section.
+            uint hdrLinkOffset = 0x10 + maskSize;
+            uint hdrChunkOffset = hdrLinkOffset + linkSize;
+
+            // Write header.
+            bw.WriteStr("Yay0");
+
+            // Write offsets.
+            bw.WriteU32s(new uint[] { (uint)data.Length, hdrLinkOffset, hdrChunkOffset });
+
+            // Copy each section into the buffer.
+            Buffer.BlockCopy(maskBuffer, 0, buffer, 0x10, (int)maskSize);
+            Buffer.BlockCopy(linkBuffer, 0, buffer, (int)hdrLinkOffset, (int)linkSize);
+            Buffer.BlockCopy(chunkBuffer, 0, buffer, (int)hdrChunkOffset, (int)chunkSize);
+
+            // Return the compressed data.
+            return new MemoryStream(buffer);
+        }
+
+        /// <summary>
+        /// Decompressing data with Yay0. Full credits for this snippet goes to Daniel McCarthy:
         /// https://github.com/Daniel-McCarthy/Mr-Peeps-Compressor/blob/master/PeepsCompress/PeepsCompress/Algorithm%20Classes/YAY0.cs
         /// </summary>
         /// <param name="stream">Stream containing the Yay0 compressed data.</param>
@@ -99,7 +260,7 @@ namespace Dolhouse.Compression
                         // Read the second byte of the compressed data.
                         byte byte2 = br.Read();
 
-                        // Advance the compressed data offset by 2 (Since we just read 2 bytes)
+                        // Advance the compressed data offset by 2.
                         compressedOffset += 2;
 
                         // Get 4 upper bits of the first byte.
@@ -111,7 +272,7 @@ namespace Dolhouse.Compression
                         int finalOffset = ((byte1Upper << 8) | byte2) + 1;
                         int finalLength;
 
-                        // Compressed chunk length is higher than 17.
+                        // Compressed chunk length is larger than 17.
                         if (byte1Lower == 0)
                         {
                             stream.Seek(decompressedOffset, SeekOrigin.Begin);
@@ -120,7 +281,7 @@ namespace Dolhouse.Compression
                             // Advance the decompressed offset by one byte.
                             decompressedOffset++;
                         }
-                        // Compressed chunk length is lower or equal to 17.
+                        // Compressed chunk length is smaller or equal to 17.
                         else
                         {
                             // The data chunk length is equals to the first byte's 4 lower bits + 2.
