2c569c66e3005da71fefee0d4e48b72863f4f1a8
nya <opeyx@pm.me>
2020-01-04 17:54:01 +0100

    Added custom classes for Vec4 and Mat4.
    The new classes are considered work in progress.

---
+++ Dolhouse/Dolhouse/Type/Mat4.cs
@@ -0,0 +1,71 @@
+﻿namespace Dolhouse.Type
+{
+
+    /// <summary>
+    /// Custom Matrix4
+    /// TODO: Proper formatting for ToString().
+    /// </summary>
+    public class Mat4
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The first Vec4 of the Mat4.
+        /// </summary>
+        public Vec4 Row1 { get; set; }
+
+        /// <summary>
+        /// The second Vec4 of the Mat4.
+        /// </summary>
+        public Vec4 Row2 { get; set; }
+
+        /// <summary>
+        /// The third Vec4 of the Mat4.
+        /// </summary>
+        public Vec4 Row3 { get; set; }
+
+        /// <summary>
+        /// The fourth Vec4 of the Mat4.
+        /// </summary>
+        public Vec4 Row4 { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Initialize a new empty Mat4.
+        /// </summary>
+        public Mat4()
+        {
+            Row1 = new Vec4();
+            Row2 = new Vec4();
+            Row3 = new Vec4();
+            Row4 = new Vec4();
+        }
+
+        /// <summary>
+        /// Initialize a new Vec4 by four Vec4's.
+        /// </summary>
+        /// <param name="row1">Value to set row1 to.</param>
+        /// <param name="row2">Value to set row2 to.</param>
+        /// <param name="row3">Value to set row3 to.</param>
+        /// <param name="row4">Value to set row4 to.</param>
+        public Mat4(Vec4 row1, Vec4 row2, Vec4 row3, Vec4 row4)
+        {
+            Row1 = row1;
+            Row2 = row2;
+            Row3 = row3;
+            Row4 = row4;
+        }
+
+        /// <summary>
+        /// Return the Mat4 as a string.
+        /// </summary>
+        /// <returns>The Mat4 formatted as a string.</returns>
+        public override string ToString()
+        {
+            return "(" + Row1.ToString() + ", " + Row2.ToString() + ", " + Row3.ToString() + ", " + Row4.ToString() + ")";
+        }
+    }
+}
---
+++ Dolhouse/Dolhouse/Type/Vec4.cs
@@ -0,0 +1,95 @@
+﻿namespace Dolhouse.Type
+{
+
+    /// <summary>
+    /// Custom Vector4
+    /// </summary>
+    public class Vec4
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The X value of the Vec4.
+        /// </summary>
+        public float X { get; set; }
+
+        /// <summary>
+        /// The Y value of the Vec4.
+        /// </summary>
+        public float Y { get; set; }
+
+        /// <summary>
+        /// The Z value of the Vec4.
+        /// </summary>
+        public float Z { get; set; }
+
+        /// <summary>
+        /// The W value of the Vec4.
+        /// </summary>
+        public float W { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Initialize a new empty Vec4.
+        /// </summary>
+        public Vec4()
+        {
+            X = 0.0f;
+            Y = 0.0f;
+            Z = 0.0f;
+            W = 0.0f;
+        }
+
+        /// <summary>
+        /// Initialize a new Vec4 by a single value.
+        /// </summary>
+        /// <param name="value">Value to set X, Y, Z and W to.</param>
+        public Vec4(float value)
+        {
+            X = value;
+            Y = value;
+            Z = value;
+            W = value;
+        }
+
+        /// <summary>
+        /// Initialize a new Vec4 by a single vector and float w.
+        /// </summary>
+        /// <param name="vector">The vector used for X, Y and Z.</param>
+        /// <param name="vector">The float used for W.</param>
+        public Vec4(Vec3 vector, float w)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+            W = w;
+        }
+
+        /// <summary>
+        /// Initialize a new Vec4 by three values.
+        /// </summary>
+        /// <param name="x">Value to set X to.</param>
+        /// <param name="y">Value to set Y to.</param>
+        /// <param name="z">Value to set Z to.</param>
+        /// <param name="w">Value to set W to.</param>
+        public Vec4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        /// <summary>
+        /// Return the Vec4 as a string.
+        /// </summary>
+        /// <returns>The Vec4 formatted as a string.</returns>
+        public override string ToString()
+        {
+            return "(" + X.ToString("n6") + ", " + Y.ToString("n6") + ", " + Z.ToString("n6") + ", " + W.ToString("n6") + ")";
+        }
+    }
+}
