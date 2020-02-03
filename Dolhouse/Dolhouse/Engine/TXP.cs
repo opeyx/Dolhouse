f76d6c78dad37b4f5237e5c7d4143440c5ae88dc
nya <opeyx@pm.me>
2020-02-03 22:56:38 +0100

    Completed TXP reading and writing functionality.

--- Dolhouse/Dolhouse/Engine/TXP.cs
+++ Dolhouse/Dolhouse/Engine/TXP.cs
@@ -1,62 +1,92 @@
 using Dolhouse.Binary;
 using System.Collections.Generic;
 using System.IO;
-using System;
 
 namespace Dolhouse.Engine
 {
 
+    /// <summary>
+    /// (T)e(X)ture (P)attern 
+    /// </summary>
     public class TXP 
     {
 
         #region Properties
-        
+
+        /// <summary>
+        /// Unknown 1.
+        /// <summary>
+        public ushort Unknown1 { get; set; }
+
+        /// <summary>
+        /// Unknown 2.
+        /// <summary>
+        public ushort Unknown2 { get; set; }
+
+        /// <summary>
+        /// Entry Count.
+        /// <summary>
+        public ushort EntryCount { get; set; }
+
+        /// <summary>
+        /// KeyFrame Count.
+        /// <summary>
+        public ushort KeyFrameCount { get; set; }
+
+        /// <summary>
+        /// KeyFrame Offset.
+        /// <summary>
+        public uint KeyFrameOffset { get; set; }
+
         /// <summary>
         /// List of entries.
         /// <summary>
-        public List<TXPEntry> TXPEntries { get; set; }
+        public List<Entry> Entries { get; set; }
 
-        /// <summary>
-        /// Frame count used for every entry
-        /// <summary>
-        public ushort FrameCount { get; set; }
-        
         #endregion
 
+
+        /// <summary>
+        /// Reads TXP from a data stream.
+        /// </summary>
+        /// <param name="stream">The stream containing the TXP data.</param>
         public TXP(Stream stream)
         {
 
+            // Define a new binary reader to read with.
             DhBinaryReader br = new DhBinaryReader(stream, DhEndian.Big);
 
-            //Ensure that Uknown 1 and 2 are 1 and 0 respectivley
-            if(br.ReadU16() != 1)
+            // Read Unknown 1.
+            Unknown1 = br.ReadU16();
+
+            // Read Unknown 2.
+            Unknown2 = br.ReadU16();
+
+            // Read Entry Count.
+            EntryCount = br.ReadU16();
+
+            // Read Keyframe Count.
+            KeyFrameCount = br.ReadU16();
+
+            // Read Keyframe Offset.
+            KeyFrameOffset = br.ReadU32();
+
+            // Initialize the list to hold our entries.
+            Entries = new List<Entry>();
+
+            // Loop through entries.
+            for(int i = 0; i < EntryCount; i++)
             {
-                throw new Exception("Unknown1 wasn't 1!");
+
+                // Read entry.
+                Entries.Add(new Entry(br, KeyFrameCount));
             }
-            
-            if(br.ReadU16() != 0)
-            {
-                throw new Exception("Unknown2 wasn't 0!");
-            }
-        
-            //Read the number of entries in the txp file
-            ushort entryCount = br.ReadU16();
-
-            //Read the number of frames in each entry
-            FrameCount = br.ReadU16();
-
-            //Skip unused frame offset
-            br.ReadU32();
-
-            TXPEntries = new List<TXPEntry>();
-
-            for(int entry = 0; entry < entryCount; entry++)
-            {
-                TXPEntries.Add(new TXPEntry(br, FrameCount));
-            }
-
         }
 
+        /// <summary>
+        /// Creates a stream from this TXP.
+        /// </summary>
+        /// <returns>The TXP as a stream.</returns>
         public Stream Write()
         {
             // Buffer for new TXP File
@@ -65,119 +95,158 @@ namespace Dolhouse.Engine
             // Define a binary writer to write with.
             DhBinaryWriter bw = new DhBinaryWriter(stream, DhEndian.Big);
 
-            // Write the fixed Unknown values
-            bw.WriteU16(1);
-            bw.WriteU16(0);
+            // Write Unknown 1.
+            bw.WriteU16(Unknown1);
 
-            // Write Entry Count
-            bw.WriteU16((ushort)TXPEntries.Count);
+            // Write Unknown 2.
+            bw.WriteU16(Unknown2);
 
-            // Write Frame Count
-            bw.WriteU16(FrameCount);
+            // Write Entry Count.
+            bw.WriteU16((ushort)Entries.Count);
 
-            //Calculate and write frame data offset
-            bw.WriteU32((uint)(0xC * TXPEntries.Count));
+            // Write Keyframe Count.
+            bw.WriteU16(KeyFrameCount);
 
-            bw.SaveOffset(0);
+            // Write Keyframe Offset.
+            bw.WriteU32(KeyFrameOffset);
 
-            //Write dummy entry headers
-            for(int i = 0; i < TXPEntries.Count; i++){
-                bw.WriteU64(0);
-                bw.WriteU32(0);
-            }
-
-            //Make a list to store the offsets for each entry's frame data
-            List<long> frameDataOffsets = new List<long>();
-
-            //Write the entries
-            for(int entry = 0; entry < TXPEntries.Count; entry++)
+            // Loop through entries.
+            for(int i = 0; i < Entries.Count; i++)
             {
-                TXPEntries[entry].Write(bw, frameDataOffsets);
+
+                // Write entry header.
+                Entries[i].WriteHeader(bw);
             }
 
-            bw.LoadOffset(0);
-            //Write frame data offsets
-            for(int i = 0; i < TXPEntries.Count; i++){
-                bw.WriteU16(1);
-                bw.WriteS16(TXPEntries[i].MaterialIndex);
-                bw.WriteU32(0);
-                bw.WriteU32((uint)frameDataOffsets[i]);
+            // Define a list to hold the offsets for each keyframe offsets.
+            List<uint> indicesOffsets = new List<uint>();
+
+            // Loop through entries.
+            for (int i = 0; i < Entries.Count; i++)
+            {
+
+                // Save the current offset
+                indicesOffsets.Add((uint)bw.Position());
+
+                // Write entry indices.
+                Entries[i].WriteIndices(bw);
             }
 
-            // Returns the TXP as a stream
+            // Loop through entries.
+            for (int i = 0; i < Entries.Count; i++)
+            {
+
+                // Go to current entry's indices offset value.
+                bw.Goto(0x0C + (i * 12) + 8);
+
+                // Write indices offset.
+                bw.WriteU32(indicesOffsets[i]);
+            }
+
+            // Return to end of file.
+            bw.Back(0);
+
+            // Write padding to nearest whole 32 bytes.
+            bw.WritePadding32();
+
+            // Return the TXP as a stream
             return stream;
         }
-
-
     }    
 
     /// <summary>
-    /// Entry in TXP File
+    /// TXP Entry
     /// </summary>
-    public class TXPEntry
+    public class Entry
     {
 
         #region Properties
 
         /// <summary>
-        /// Unknown Short
+        /// Unknown 1.
         /// </summary>
         public short Unknown1 { get; set; }
 
         /// <summary>
-        /// Material Index
+        /// Material Index.
         /// </summary>
         public short MaterialIndex { get; set; }
 
         /// <summary>
-        /// MDL TexObj indicies
+        /// Unknown 2.
         /// </summary>
-        public short[] TexObjIndicies { get; set; }
+        public int Unknown2 { get; set; }
+
+        /// <summary>
+        /// Indices Offset.
+        /// </summary>
+        public uint IndicesOffset { get; set; }
+
+        /// <summary>
+        /// MDL Texture Objects Indices.
+        /// </summary>
+        public short[] Indices { get; set; }
 
         #endregion
 
-        public TXPEntry(DhBinaryReader br, ushort frameCount)
+
+        /// <summary>
+        /// Read a single entry in TXP.
+        /// </summary>
+        /// <param name="br">The binaryreader to read with.</param>
+        /// <param name="keyFrameCount">The amount of keyframes in each entry.</param>
+        public Entry(DhBinaryReader br, ushort keyFrameCount)
         {
 
-            //Ensure that Uknown 1 is 1
-            if(br.ReadU16() != 1)
-            {
-                throw new Exception("Unknown1 wasn't 1!");
-            }
+            // Read Unknown 1.
+            Unknown1 = br.ReadS16();
             
-            //Read material index
+            //Read Material Index.
             MaterialIndex = br.ReadS16();
 
-            if(br.ReadU32() != 0)
-            {
-                throw new Exception("Unknown2 wasn't 0!");
-            }
+            // Read Unknown 2.
+            Unknown2 = br.ReadS32();
 
-            //Read the frame data offset for this 
-            uint frameDataOffset = br.ReadU32();
+            // Read Indices Offset.
+            IndicesOffset  = br.ReadU32();
 
-            TexObjIndicies = new short[frameCount];
-
-            //Save reader's current position and seek to the frame data
-            br.SaveOffset(0);
-            br.Goto(frameDataOffset);
-
-            //Fill TexObjIndicies for each frame
-            for(int frame = 0; frame < frameCount; frame++) 
-            {
-                TexObjIndicies[frame] = br.ReadS16();
-            }
-
-            br.LoadOffset(0);
-
+            // Read indices.
+            Indices = br.ReadS16sAt(IndicesOffset, keyFrameCount);
         }
 
-        public void Write(DhBinaryWriter bw, List<long> frameDataOffsets)
+        /// <summary>
+        /// Write a single entry header.
+        /// </summary>
+        /// <param name="bw">The binarywriter to write with.</param>
+        public void WriteHeader(DhBinaryWriter bw)
         {
-            frameDataOffsets.Add(bw.GetStream().Position);
-            for(int i = 0; i < TexObjIndicies.Length; i++)
+
+            // Write Unknown 1.
+            bw.WriteS16(Unknown1);
+
+            // Write Material Index.
+            bw.WriteS16(MaterialIndex);
+
+            // Write Unknown 2.
+            bw.WriteS32(Unknown2);
+
+            // Write Indices Offset.
+            bw.WriteU32(IndicesOffset);
+        }
+
+        /// <summary>
+        /// Write entry data.
+        /// </summary>
+        /// <param name="bw">The binarywriter to write with.</param>
+        public void WriteIndices(DhBinaryWriter bw)
+        {
+
+            // Loop through indices.
+            for (int i = 0; i < Indices.Length; i++)
             {
-                bw.WriteS16(TexObjIndicies[i]);
+
+                // Write index.
+                bw.WriteS16(Indices[i]);
             }
         }
     }
