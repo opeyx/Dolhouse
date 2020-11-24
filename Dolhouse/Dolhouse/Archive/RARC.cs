f9b4b605e2f5bf8e078fdfd95a4bf23df009917b
opeyx <opeyx@pm.me>
2020-11-24 20:19:04 +0100

    Added code for RARC reading.
    This is pretty crude and dirty and should therefore ideally be refactored.

--- Dolhouse/Dolhouse/Archive/RARC.cs
+++ Dolhouse/Dolhouse/Archive/RARC.cs
@@ -1,490 +1,154 @@
 ﻿using Dolhouse.Binary;
+using Dolhouse.Compression;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace Dolhouse.Archive
 {
-
-    /// <summary>
-    /// RARC Archive
-    /// </summary>
-    public class RARC
+    /// Many thanks to Sage-Of-Mirrors/LordNed for code reference:
+    /// https://github.com/LordNed/WArchive-Tools/
+    public static class RARC
     {
-
-        #region Properties
-        
-        /// <summary>
-        /// RARC Header.
-        /// </summary>
-        public RARCHeader Header { get; set; }
-
-        /// <summary>
-        /// RARC Info Block.
-        /// </summary>
-        public RARCInfoBlock InfoBlock { get; set; }
-
-        /// <summary>
-        /// List of nodes stored in RARC.
-        /// </summary>
-        public List<RARCNode> Nodes { get; set; }
-
-        #endregion
-
-
-        /// <summary>
-        /// Reads RARC from a data stream.
-        /// </summary>
-        /// <param name="stream">The stream containing the RARC data.</param>
-        public RARC(Stream stream)
+        public static VirtDirectory LoadRarc(byte[] data)
         {
-
-            // Define a binary reader to read with.
-            DhBinaryReader br = new DhBinaryReader(stream, DhEndian.Big);
-
-            // Read node's header.
-            Header = new RARCHeader(br);
-
-            // Read node's info block.
-            InfoBlock = new RARCInfoBlock(br);
-
-            // Define a new list to hold the RARC's nodes.
-            Nodes = new List<RARCNode>();
-
-            // Loop through RARC's nodes.
-            for (uint i = 0; i < InfoBlock.NodeCount; i++)
+            if (Yay0.IsCompressed(data))
             {
-
-                // Read node and add it to the list of nodes.
-                Nodes.Add(new RARCNode(br, Header, InfoBlock));
+                data = Yay0.Decompress(data);
             }
+
+            DhBinaryReader br = new DhBinaryReader(data, DhEndian.Big);
+
+            if (br.ReadStr(4) != "RARC") throw new InvalidDataException("No valid RARC signature was found!");
+
+            List<RarcNode> nodes = new List<RarcNode>();
+            List<RarcEntry> entries = new List<RarcEntry>();
+
+            // read header
+            // RARC here
+            br.Skip(4);
+            br.Skip(4);
+            var dataOffset = br.ReadU32() + 0x20;
+            br.Skip(4);
+            br.Skip(4);
+            br.Skip(4);
+            br.Skip(4);
+
+            // read infoblock
+            var NodeCount = br.ReadU32();
+            var NodeOffset = br.ReadU32() + 0x20;
+            var EntryCount = br.ReadU32();
+            var EntryOffset = br.ReadU32() + 0x20;
+            br.Skip(4);
+            var StringTableOffset = br.ReadU32() + 0x20;
+            br.Skip(2);
+            br.Skip(2);
+            br.Skip(4);
+
+            br.Goto(EntryOffset);
+
+            // read entries
+            for (int i = 0; i < EntryCount; i++)
+            {
+                RarcEntry entry = new RarcEntry()
+                {
+                    Id = br.ReadU16(),
+                    NameHash = br.ReadU16(),
+                    Type = br.ReadU16(),
+                    NameOffset = br.ReadU16(),
+                    DataOffset = br.ReadU32(),
+                    DataLength = br.ReadU32()
+                };
+
+                if (entry.Type == 0x1100)
+                {
+                    entry.Data = br.ReadAt(dataOffset + entry.DataOffset, (int)entry.DataLength);
+                }
+                entry.MemoryPointer = br.ReadU32();
+                entry.Name = br.ReadStrAt(StringTableOffset + entry.NameOffset);
+
+                entries.Add(entry);
+            }
+
+            br.Goto(NodeOffset);
+
+            // read nodes
+            for (int i = 0; i < NodeCount; i++)
+            {
+                RarcNode rarcNode = new RarcNode()
+                {
+                    Id = br.ReadStr(4),
+                    NameOffset = br.ReadU32(),
+                    NameHash = br.ReadU16(),
+                    EntryCount = br.ReadU16(),
+                    FirstEntryIndex = br.ReadU32()
+                };
+
+                rarcNode.Name = br.ReadStrAt(StringTableOffset + rarcNode.NameOffset);
+                rarcNode.Entries = entries.GetRange((int)rarcNode.FirstEntryIndex, (int)rarcNode.EntryCount);
+
+                nodes.Add(rarcNode);
+            }
+
+            List<VirtDirectory> virtDirectories = new List<VirtDirectory>(nodes.Count);
+            foreach (RarcNode rarcNode in nodes)
+            {
+                virtDirectories.Add(new VirtDirectory(rarcNode.Name, Guid.Empty));
+            }
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                RarcNode rarcNode = nodes[i];
+
+                for (int y = 0; y < nodes[i].Entries.Count; y++)
+                {
+
+                    if (rarcNode.Entries[y].Name == "." || rarcNode.Entries[y].Name == "..")
+                        continue;
+
+                    if (rarcNode.Entries[y].Type == (ushort)NodeType.Directory)
+                    {
+                        var virtDirectory = virtDirectories[(int)rarcNode.Entries[y].DataOffset];
+                        virtDirectory.ParentGuid = virtDirectories[i].Guid;
+                        virtDirectory.Name = rarcNode.Entries[y].Name;
+
+                        virtDirectories[i].Children.Add(virtDirectory);
+                    }
+                    else
+                    {
+                        VirtFile virtFile = new VirtFile(rarcNode.Entries[y].Name, virtDirectories[i].Guid, rarcNode.Entries[y].Data);
+                        virtDirectories[i].Children.Add(virtFile);
+                    }
+                }
+            }
+
+            return virtDirectories.Count > 0 ? virtDirectories[0] : null;
         }
     }
 
-    /// <summary>
-    /// RARC Header
-    /// </summary>
-    public class RARCHeader
+    public class RarcNode
     {
-
-        #region Properties
-        
-        /// <summary>
-        /// RARC Magic.
-        /// </summary>
-        public string Magic { get; set; }
-
-        /// <summary>
-        /// Total length of file.
-        /// </summary>
-        public uint FileLength { get; set; }
-
-        /// <summary>
-        /// Total length of header.
-        /// </summary>
-        public uint HeaderLength { get; set; }
-
-        /// <summary>
-        /// Offset to data.
-        /// </summary>
-        public uint DataOffset { get; }
-
-        /// <summary>
-        /// Total length of data.
-        /// </summary>
-        public uint DataLength { get; set; }
-
-        /// <summary>
-        /// Unknown 1. (Same as DataLength?)
-        /// </summary>
-        public uint Unknown1 { get; set; }
-
-        /// <summary>
-        /// Unknown 2.
-        /// </summary>
-        public uint Unknown2 { get; set; }
-
-        /// <summary>
-        /// Unknown 3.
-        /// </summary>
-        public uint Unknown3 { get; set; }
-
-        #endregion
-
-
-        /// <summary>
-        /// Read RARC Header.
-        /// </summary>
-        /// <param name="br">Binary Reader to use.</param>
-        public RARCHeader(DhBinaryReader br)
-        {
-
-            // Read magic.
-            Magic = br.ReadStr(4);
-
-            // Check magic and throw an exception if invalid.
-            if (Magic != "RARC") throw new InvalidDataException("No valid RARC signature was found!");
-
-            // Read total file length.
-            FileLength = br.ReadU32();
-
-            // Read length of header.
-            HeaderLength = br.ReadU32();
-
-            // Read offset to data offset. (Relative to RARCHeader)
-            DataOffset = br.ReadU32() + 0x20;
-
-            // Read length of data.
-            DataLength = br.ReadU32();
-
-            // Read unknown 1.
-            Unknown1 = br.ReadU32();
-
-            // Read unknown 2.
-            Unknown2 = br.ReadU32();
-
-            // Read unknown 3.
-            Unknown3 = br.ReadU32();
-        }
-    }
-
-    /// <summary>
-    /// RARC Info Block
-    /// </summary>
-    public class RARCInfoBlock
-    {
-
-        #region Properties
-        
-        /// <summary>
-        /// Total amount of nodes.
-        /// </summary>
-        public uint NodeCount { get; set; }
-        
-        /// <summary>
-        /// Offset to the first node.
-        /// </summary>
-        public uint FirstNodeOffset { get; set; }
-
-        /// <summary>
-        /// Total amount of directories.
-        /// </summary>
-        public uint DirectoryCount { get; set; }
-
-        /// <summary>
-        /// Offset to the first directory.
-        /// </summary>
-        public uint FirstDirectoryOffset { get; set; }
-        
-        /// <summary>
-        /// Total length of StringTable.
-        /// </summary>
-        public uint StringTableLength { get; set; }
-
-        /// <summary>
-        /// Offset to the string table.
-        /// </summary>
-        public uint StringTableOffset { get; set; }
-
-        /// <summary>
-        /// The amount of directories that are files.
-        /// </summary>
-        public ushort DirectoryFileCount { get; set; }
-
-        /// <summary>
-        /// Unknown 1. (Always 0?)
-        /// </summary>
-        public ushort Unknown1 { get; set; }
-
-        /// <summary>
-        /// Unknown 2. (Always 0?)
-        /// </summary>
-        public uint Unknown2 { get; set; }
-
-        #endregion
-
-        /// <summary>
-        /// Read RARC InfoBlock.
-        /// </summary>
-        /// <param name="br">Binary Reader to use.</param>
-        public RARCInfoBlock(DhBinaryReader br)
-        {
-
-            // Read amount of nodes.
-            NodeCount = br.ReadU32();
-
-            // Read offset to first node. (Relative to InfoBlock)
-            FirstNodeOffset = br.ReadU32() + 0x20;
-
-            // Read amount of entries.
-            DirectoryCount = br.ReadU32();
-
-            // Read offset to first directory. (Relative to InfoBlock)
-            FirstDirectoryOffset = br.ReadU32() + 0x20;
-
-            // Read total length of string table.
-            StringTableLength = br.ReadU32();
-
-            // Read offset to string table. (Relative to InfoBlock)
-            StringTableOffset = br.ReadU32() + 0x20;
-
-            // Read amount of directories that are files.
-            DirectoryFileCount = br.ReadU16();
-
-            // Read unknown 1.
-            Unknown1 = br.ReadU16();
-
-            // Read unknown 2.
-            Unknown2 = br.ReadU32();
-        }
-    }
-
-    
-    /// <summary>
-    /// RARC Node
-    /// </summary>
-    public class RARCNode
-    {
-
-        #region Properties
-        
-        /// <summary>
-        /// Node indentifier.
-        /// </summary>
         public string Id { get; set; }
-
-        /// <summary>
-        /// Node's name offset in string table.
-        /// </summary>
         public uint NameOffset { get; set; }
-
-        /// <summary>
-        /// Node's name hash.
-        /// </summary>
-        public ushort Hash { get; set; }
-
-        /// <summary>
-        /// Node's amount of directories.
-        /// </summary>
-        public ushort DirectoryCount { get; set; }
-
-        /// <summary>
-        /// Index of node's first directory.
-        /// </summary>
-        public uint FirstDirectoryIndex { get; set; }
-
-        /// <summary>
-        /// List of directories in node.
-        /// </summary>
-        public List<RARCDirectory> Directories { get; set; }
-
-        /// <summary>
-        /// Node's name.
-        /// </summary>
+        public ushort NameHash { get; set; }
+        public ushort EntryCount { get; set; }
+        public uint FirstEntryIndex { get; set; }
         public string Name { get; set; }
-
-        #endregion
-
-
-        /// <summary>
-        /// Read a RARCNode from RARC.
-        /// </summary>
-        /// <param name="br">Binary Reader to use.</param>
-        /// <param name="header">The RARC's Header.</param>
-        /// <param name="infoBlock">The RARC's InfoBlock.</param>
-        public RARCNode(DhBinaryReader br, RARCHeader header, RARCInfoBlock infoBlock)
-        {
-
-            // Read node's identifier.
-            Id = br.ReadStr(4);
-
-            // Read node's name offset in string table.
-            NameOffset = br.ReadU32();
-
-            // Read node's name hash.
-            Hash = br.ReadU16();
-
-            // Read node's amount of directories.
-            DirectoryCount = br.ReadU16();
-
-            // Read node's first directory's index.
-            FirstDirectoryIndex = br.ReadU32();
-
-            // Save the current position offset.
-            long currentPosition = br.Position();
-
-            // Define a new list to hold the RARCNode's directories.
-            Directories = new List<RARCDirectory>();
-
-            // Loop through RARCNode's directories.
-            for (int i = 0; i < DirectoryCount; i++)
-            {
-                // Read directory and add it to the node's list of directories.
-                Directories.Add(new RARCDirectory(br, header, infoBlock));
-            }
-
-            // Go to the string table offset + the current node's name offset.
-            br.Goto(infoBlock.StringTableOffset + NameOffset);
-
-            // Read the node's name as a null-terminated string.
-            Name = br.ReadStr();
-
-            // Go back to the position we previously saved.
-            br.Goto(currentPosition);
-        }
+        public List<RarcEntry> Entries { get; set; }
     }
 
-
-    /// <summary>
-    /// RARC Directory
-    /// </summary>
-    public class RARCDirectory
+    public class RarcEntry
     {
-
-        #region Properties
-
-        /// <summary>
-        /// Directory's index.
-        /// </summary>
-        public ushort Index { get; set; }
-
-        /// <summary>
-        /// Directory's hash.
-        /// </summary>
-        public ushort Hash { get; set; }
-
-        /// <summary>
-        /// Directory's type.
-        /// </summary>
+        public ushort Id { get; set; }
+        public ushort NameHash { get; set; }
         public ushort Type { get; set; }
-
-        /// <summary>
-        /// Directory's name offset.
-        /// </summary>
         public ushort NameOffset { get; set; }
-
-        /// <summary>
-        /// Directory's data offset (if file) / node index (if directory).
-        /// </summary>
-        public uint DataOffsetOrNodeIndex { get; set; } // relative to RarcHeader.DataOffset
-        
-        /// <summary>
-        /// Directory's data length (if file) / 0 (if directory).
-        /// </summary>
+        public uint DataOffset { get; set; } // If this is a directory, the is the index of the directory's node. If it's a file, it's the offset to the file's data. 
         public uint DataLength { get; set; }
+        public uint MemoryPointer { get; set; }
 
-        /// <summary>
-        /// Unknown 1 (Always 0?)
-        /// </summary>
-        public uint Unknown1 { get; set; }
-
-        /// <summary>
-        /// Directory's name.
-        /// </summary>
         public string Name { get; set; }
-
-        /// <summary>
-        /// Directory's data.
-        /// </summary>
         public byte[] Data { get; set; }
-
-        /// <summary>
-        /// Flag to determine if directory is a folder.
-        /// </summary>
-        public bool IsFolder { get; set; }
-
-        #endregion
-
-
-        /// <summary>
-        /// Read a Directory from RARC.
-        /// </summary>
-        /// <param name="br">Binary Reader to use.</param>
-        /// <param name="header">The RARC's Header.</param>
-        /// <param name="infoBlock">The RARC's InfoBlock.</param>
-        public RARCDirectory(DhBinaryReader br, RARCHeader header, RARCInfoBlock infoBlock)
-        {
-
-            // Read directory's index.
-            Index = br.ReadU16();
-
-            // Read directory's hash.
-            Hash = br.ReadU16();
-
-            // Read directory's type.
-            Type = br.ReadU16();
-
-            // Read directory's name offset.
-            NameOffset = br.ReadU16();
-
-            // Read directory's data offset / node index.
-            DataOffsetOrNodeIndex = br.ReadU32();
-
-            // Read directory's data length / 0.
-            DataLength = br.ReadU32();
-
-            // Read unknown 1.
-            Unknown1 = br.ReadU32();
-
-            // Save the current position offset.
-            long currentPosition = br.Position();
-
-            // Go to the string table offset + the current node's name offset.
-            br.Goto(infoBlock.StringTableOffset + NameOffset);
-
-            // Read the node's name as a null-terminated string.
-            Name = br.ReadStr();
-
-            // Determine if this Directory is a folder.
-            IsFolder = (Type == 0x1100 && Index != 0xFFFF) ? false : true;
-
-            // Check if directory is NOT a folder.
-            if (!IsFolder)
-            {
-                // Go to data offset + current directory's data offset.
-                br.Goto(header.DataOffset + DataOffsetOrNodeIndex);
-
-                // Read current directory's data.
-                Data = br.Read((int)DataLength);
-            }
-
-            // Go back to the position we previously saved.
-            br.Goto(currentPosition);
-        }
-    }
-
-
-    /// <summary>
-    /// RARC Utilities
-    /// </summary>
-    public class RARCUtils
-    {
-
-        /// <summary>
-        /// RARC Name Hashing Method.
-        /// Full credits to Wexos:
-        /// http://wiki.tockdom.com/w/index.php?title=RARC_(File_Format)&oldid=117549
-        /// </summary>
-        /// <param name="name">Name to hash.</param>
-        /// <returns>Hashed hame as a unsigned short.</returns>
-        public static ushort Hash(string name)
-        {
-
-            // Define variable to hold our hash value.
-            ushort Hash = 0;
-
-            // Loop through each character of our name.
-            for (int i = 0; i < name.Length; i++)
-            {
-                // Multiple the hash value by 3.
-                Hash *= 3;
-
-                // Add the current character's value to the  hash value.
-                Hash += Encoding.ASCII.GetBytes(new char[1] { name[i] })[0];
-            }
-
-            // Return the calculated hash.
-            return Hash;
-        }
     }
 }
---
+++ Dolhouse/Dolhouse/Archive/VirtFS.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dolhouse.Archive
+{
+    public class VirtNode
+    {
+        public Guid Guid { get; set; }
+        public Guid ParentGuid { get; set; }
+        public string Name { get; set; }
+        public NodeType Type { get; set; }
+
+        public VirtNode(string name, NodeType type, Guid parentGuid)
+        {
+            Guid = Guid.NewGuid();
+            Name = name;
+            Type = type;
+            ParentGuid = parentGuid;
+        }
+    }
+
+    public class VirtDirectory : VirtNode
+    {
+        public List<VirtNode> Children { get; set; }
+
+        public VirtDirectory(string path) : base(Path.GetFileName(path), NodeType.Directory, Guid.Empty)
+        {
+            Children = new List<VirtNode>();
+
+            string[] files = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
+            string[] directories = Directory.GetDirectories(path, "**", SearchOption.TopDirectoryOnly);
+
+            foreach (string file in files)
+            {
+                Children.Add(new VirtFile(file)
+                {
+                    ParentGuid = Guid
+                });
+            }
+
+            foreach (string directory in directories)
+            {
+                Children.Add(new VirtDirectory(directory)
+                {
+                    ParentGuid = Guid
+                });
+            }
+        }
+
+        public VirtDirectory(string name, Guid parentGuid) : base(Path.GetFileName(name), NodeType.Directory, parentGuid)
+        {
+            Children = new List<VirtNode>();
+        }
+
+        public VirtDirectory(string name, Guid parentGuid, List<VirtNode> children) : base(Path.GetFileName(name), NodeType.Directory, parentGuid)
+        {
+            Children = children;
+        }
+
+        public VirtNode GetNodeByGuid(Guid guid, bool recursive = false)
+        {
+            foreach (VirtNode node in GetNodes(recursive))
+            {
+                if (node.Guid == guid)
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        public VirtFile GetFileByPath(string path, bool recursive = true)
+        {
+            // TODO - Complete this!
+            string[] names = path.Split('\\');
+
+            if (names.Length < 1)
+            {
+                foreach (VirtFile file in GetFiles())
+                {
+
+                }
+                return null;
+            }
+
+            foreach (VirtFile file in GetNodes(recursive))
+            {
+                if ((file.Name + file.Extension) == path)
+                {
+                    return file;
+                }
+                else if (file.Name == path)
+                {
+                    return file;
+                }
+            }
+            return null;
+        }
+
+        public VirtFile GetFileByName(string name, bool recursive = true)
+        {
+            foreach (VirtFile file in GetFiles(recursive))
+            {
+                if ((file.Name + file.Extension) == name)
+                {
+                    return file;
+                }
+                else if (file.Name == name)
+                {
+                    return file;
+                }
+            }
+            return null;
+        }
+
+        public VirtDirectory GetDirectoryByName(string name, bool recursive = true)
+        {
+            foreach (VirtDirectory directory in GetDirectories(recursive))
+            {
+                if (directory.Name == name)
+                {
+                    return directory;
+                }
+            }
+            return null;
+        }
+
+        public List<VirtFile> GetFilesByExtension(string extension, bool recursive = true)
+        {
+            List<VirtFile> files = new List<VirtFile>();
+
+            if (extension.Length == 0)
+            {
+                return files;
+            }
+
+            if (!extension.StartsWith("."))
+            {
+                extension = extension.Insert(0, ".");
+            }
+
+            foreach (VirtFile file in GetFiles(recursive))
+            {
+                if (file.Extension == extension)
+                {
+                    files.Add(file);
+                }
+            }
+            return files;
+        }
+
+        public void RemoveFileByName(string name, bool recursive = false)
+        {
+            var result = GetFileByName(name, recursive);
+            if (result != null)
+            {
+                Children.Remove(result);
+            }
+            else
+            {
+                throw new InvalidOperationException($"File '{name}' does not exist!");
+            }
+        }
+
+        public void RemoveDirectoryByName(string name, bool recursive = false)
+        {
+            var result = GetDirectoryByName(name, recursive);
+            if (result != null)
+            {
+                Children.Remove(result);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Directory '{name}' does not exist!");
+            }
+        }
+
+        public List<VirtNode> GetNodes(bool recursive = false)
+        {
+            List<VirtNode> nodes = new List<VirtNode>();
+            foreach (VirtNode node in Children)
+            {
+                nodes.Add(node);
+                if (node.Type == NodeType.Directory)
+                {
+                    if (recursive)
+                    {
+                        nodes.AddRange(((VirtDirectory)node).GetNodes(recursive));
+                    }
+                }
+            }
+            return nodes;
+        }
+
+        public List<VirtFile> GetFiles(bool recursive = false)
+        {
+            List<VirtFile> files = new List<VirtFile>();
+            foreach (VirtNode node in GetNodes(recursive))
+            {
+                if (node.Type == NodeType.File)
+                {
+                    files.Add((VirtFile)node);
+                }
+            }
+            return files;
+        }
+
+        public List<VirtDirectory> GetDirectories(bool recursive = false)
+        {
+            List<VirtDirectory> directories = new List<VirtDirectory>();
+            foreach (VirtNode node in GetNodes(recursive))
+            {
+                if (node.Type == NodeType.Directory)
+                {
+                    directories.Add((VirtDirectory)node);
+                }
+            }
+            return directories;
+        }
+
+        public void Export(string path)
+        {
+            string directoryPath = $"{path}\\{Name}";
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            foreach (VirtNode node in Children)
+            {
+                switch (node.Type)
+                {
+                    case NodeType.File:
+                        ((VirtFile)node).Export(directoryPath);
+                        break;
+                    case NodeType.Directory:
+                        ((VirtDirectory)node).Export(directoryPath);
+                        break;
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+        }
+    }
+
+    public class VirtFile : VirtNode
+    {
+        public string Extension { get; set; }
+        public byte[] Data { get; set; }
+
+        public VirtFile(string path) : base(Path.GetFileNameWithoutExtension(path), NodeType.File, Guid.Empty)
+        {
+            Extension = Path.GetExtension(path);
+            Data = File.ReadAllBytes(path);
+        }
+
+        public VirtFile(string name, Guid parentGuid, byte[] data) : base(Path.GetFileNameWithoutExtension(name), NodeType.File, parentGuid)
+        {
+            Extension = Path.GetExtension(name);
+            Data = data;
+        }
+
+        public void Export(string path)
+        {
+            string filePath = $"{path}\\{Name}{Extension}";
+            byte[] reverse = Data;
+            Array.Reverse(reverse);
+            File.WriteAllBytes(filePath, reverse);
+        }
+    }
+
+    public static class VirtUtils
+    {
+        public static VirtFile GetFileByPath(VirtDirectory directory, string path)
+        {
+            // TODO - Add funtionality..
+            return new VirtFile("test", Guid.Empty, new byte[2]);
+        }
+
+        public static string GetFullNodePath(VirtDirectory directory, VirtNode node)
+        {
+            string result = "";
+            // TODO - Add funtionality..
+            return result;
+        }
+    }
+
+    public enum NodeType
+    {
+        None,
+        Directory = 512,
+        File = 4352
+    }
+}
